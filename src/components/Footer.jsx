50a12e51913e41da04a8c1022d850e58a249fb73
Rounak Sen <103501472+rony0000013@users.noreply.github.com>
2023-04-29 15:48:26 +0000

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,49 +4,49 @@ import { FaLinkedinIn , FaGithub, FaInstagram, FaFacebook, FaTwitter} from "reac
 const Footer = () => {
     return (
       <>
-      <footer className="flex flex-row flex-wrap items-center justify-between py-1 px-6  bg-gradient-to-r from-indigo-500 via-violet-500 to-purple-500">
+      <footer className="flex md:flex-row flex-column flex-wrap items-center justify-between py-0 md:px-6  bg-gradient-to-r from-indigo-500 via-violet-500 to-purple-500 fixed bottom-0  w-full">
         <div classname='footer-socials' >
         <span style={{"gap": "1rem", "cursor": "pointer", "display": "flex"}}> 
             <div>
-                <a
-                    href="https://www.linkedin.com/company/sccsbsaot"
-                    target="_blank">
+                <a  href="https://www.linkedin.com/company/sccsbsaot"
+                    target="_blank" rel="noreferrer">
                     <FaLinkedinIn className="icons"/>
                 </a>
             </div>
             <div>
                 <a 
                 href="https://github.com/sccsbsaot"
-                target="_blank">
+                target="_blank" rel="noreferrer">
                 <FaGithub className="icons"/>
                 </a>
             </div>
             <div>
                 <a
                 href="https://instagram.com/sccsbsaot?igshid=ZDdkNTZiNTM="
-                target="_blank">
+                target="_blank" rel="noreferrer">
                 <FaInstagram className="icons"/>
                 </a>
             </div>
             <div>
                 <a
                 href="https://www.facebook.com/profile.php?id=100090904104316&mibextid=ZbWKwL"
-                target="_blank">
+                target="_blank" rel="noreferrer">
                 <FaFacebook className="icons"/>
                 </a>
             </div>
             <div>
                 <a
                 href="https://twitter.com/sccsbs"
-                target="_blank">
+                target="_blank" rel="noreferrer">
                 <FaTwitter className="icons"/>    
                 </a>
             </div>
             </span>
         </div>
-        <div className='text-center px-3'>
+        <div className='text-center px-3 '>
         &copy; {new Date().getFullYear()} Copyright:{' '}
-        <a className='text-dark'>
+        <a className='text-dark' href="https://github.com/sccsbsaot"
+                target="_blank" rel="noreferrer">
           SC_CSBS
         </a>
       </div>
