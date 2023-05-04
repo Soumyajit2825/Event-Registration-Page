e4751e7bebe634b370170af97bffd79e7fb23f8b
Rounak Sen <103501472+rony0000013@users.noreply.github.com>
2023-05-04 22:34:35 +0530

    image compress

--- src/UI/BGPattern.jsx
+++ src/UI/BGPattern.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import maleimg from "../assets/male.png"
-import femaleimg from "../assets/female.jpeg"
+import femaleimg from "../assets/female.png"
 
 
 const BGPattern = ({gender}) => {
