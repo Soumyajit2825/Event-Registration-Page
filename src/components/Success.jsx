7e74d27fa175c927d1c85771a09c36579d15c882
Rounak Sen <103501472+rony0000013@users.noreply.github.com>
2023-05-04 21:22:03 +0530

    payment message updated

--- src/components/Success.jsx
+++ src/components/Success.jsx
@@ -12,7 +12,7 @@ const Success = () => {
                 <Tadas><h2 className="text-black text-center font-extrabold md:text-8xl text-5xl px-3  pb-10">Registration Successful!</h2></Tadas>
 
                 <a href="https://docs.google.com/forms/d/1P8QWlZP9D_ZUixLyURtxqbyfEzK1jvcHCdMraMgYKJM" target="_blank" >
-                <button className='bg-white p-3 rounded-xl mt-20 md:text-2xl text-l text-black font-bold border-black border-4 hover:scale-[1.05]' >Click here to Submit the Payment Screenshot</button>
+                <h3 className='bg-white p-3 text-center rounded-xl mt-20 md:text-2xl text-md text-black font-bold border-black border-4 hover:scale-[1.05]' >Please Collect Your Payment Recipt from D-block Entrance.</h3>
                 </a>
            </div>
         </>
