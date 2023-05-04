45e3a2690a2acb08a82b81c847764325d9762937
Rounak Sen <103501472+rony0000013@users.noreply.github.com>
2023-05-05 02:38:30 +0530

    message add

--- src/components/Success.jsx
+++ src/components/Success.jsx
@@ -11,7 +11,7 @@ const Success = () => {
             <div className="flex flex-col items-center justify-center h-[100vh] w-full overflow-hidden bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500">
                 <Tadas><h2 className="text-black text-center font-extrabold md:text-8xl text-5xl px-3  pb-10">Registration Successful!</h2></Tadas>
 
-                <h3 className='bg-white p-3 text-center rounded-xl mt-20 md:text-2xl text-md text-black font-bold border-black border-4 hover:scale-[1.05]' >Please Collect Your Payment Recipt from D-block Ground Floor.</h3>
+                <h3 className='bg-white p-3 text-center rounded-xl mt-20 md:text-2xl text-md text-black font-bold border-black border-4 hover:scale-[1.05]' >Please Collect Your Payment Recipt from D-block Ground Floor.<br/> Please Check The Team Lead's Email for further updates.</h3>
                 
            </div>
         </>
